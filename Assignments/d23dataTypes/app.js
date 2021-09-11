e3e41c7021a33bba3b889885780c3152c6e135f4
Gebre20 <59422715+Gebre20@users.noreply.github.com>
2021-09-11 09:20:18 -0500

    Update app.js

--- Assignments/d23dataTypes/app.js
+++ Assignments/d23dataTypes/app.js
@@ -1,7 +1,4 @@
 "use strict";
-/* eslint-disable */
-
-/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
 module.exports = {
   ucFirst,
   getMaxSubSum,
@@ -9,7 +6,7 @@ module.exports = {
   checkSpam,
   extractCurrencyValue,
   camelize,
-}; //add all of your function names here that you need for the node mocha tests
+}; 
 
 function ucFirst(str) {
   return str.charAt(0).toUpperCase() + "" + str.slice(1);
@@ -28,21 +25,15 @@ function extractCurrencyValue(currency) {
   return Number(currency.slice(1));
 }
 
-/**
- * 
- * @param {Array} arr of numbers
- * @returns {number} the total of the maximal subarray
- 
- */
 function getMaxSubSum(arr) {
   let max = 0;
   let subSum = 0;
 
   for (let item of arr) {
     // for each item of arr
-    subSum += item; // add it to partialSum
-    max = Math.max(max, subSum); // remember the maximum
-    if (subSum < 0) subSum = 0; // zero if negative
+    subSum += item; 
+    max = Math.max(max, subSum); 
+    if (subSum < 0) subSum = 0; 
   }
 
   return max;
