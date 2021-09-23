d69c118b2cf69cdc9c4204585bd12a7a369057eb
Gebre20 <59422715+Gebre20@users.noreply.github.com>
2021-09-22 20:26:51 -0500

    Create savingsaccount.js

---
+++ Assignments/d42class/savingsaccount.js
@@ -0,0 +1,39 @@
+"use strict";
+/* global Account require  */
+const acc = require("./account.js");//with node need the name of your file with your functions here
+const Account = acc.Account;  //do this for all of the functions used in the Mocha tests
+
+/* exports at end of file since exporting an object, which can only be referenced after definition   */
+class SavingsAccount extends Account{
+    constructor(number, interest){
+        super(number);
+        this.setInteretst(interest);
+
+    }
+    addInterest(){
+        let calcInterest = (this._balance*this._interest/100);
+        return this.deposit(calcInterest);
+    }
+    getInteretst(){
+        return this._interest;
+    }
+    setInteretst(interest){
+        this._interest = interest;
+    }
+    toString(){
+        let str =  "SavingsAccount "+this._number+":"+ " balance: "+ this._balance + " interest: "+this._interest ;
+        return str;
+      }
+      endOfMonth(){
+        let report = "Interest added SavingsAccount "+this._number+":"+" balance: "+this._balance+" interest: "+this._interest;
+        return report;
+    }
+}
+
+
+
+
+
+
+/* global exports */
+exports.SavingsAccount = SavingsAccount;
